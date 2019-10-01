aee10be25a40c95c8fe4fc80a1adcb5630d70784
Alpha-Lead <Alpha-Lead@users.noreply.github.com>
2019-10-01 20:04:04 +0930

    Corrected error in code_main

--- code_main.py
+++ code_main.py
@@ -1,16 +1,16 @@
+#project_HEDONE
+#Import dependancies
 import sys #To handle passing in arguments
 import getopt #To handle argument parsing
-from code_user import code_user
-from code_subreddit import code_subreddit
 
-#getopt.getopt(args, options, [long_options])
-#options="f:u:s:"
+from code_user import code_user #To operate on users
+from code_subreddit import code_subreddit #To operate on subreddits
 
 ##Process arguments
 def main(argv):
     try:
-        options, arguments = getopt.getopt(argv, "f:u:s:h", ["file", "user", "subreddit", "help"])
-    except getopt.GetoptError as err:
+        optns, args = getopt.getopt(argv, "f:u:s:h", ["file", "user", "subreddit", "help"])
+    except getopt.GetoptError:
         print("EXCEPTION_THROWN - SECTION_arguments")
         print("Invalid arguments.")
         print("code_main.py <flag> <name/filepath>")
@@ -21,18 +21,18 @@ def main(argv):
         print("-s --subreddit | Input subreddit name")
         sys.exit(1)
     
-    for opt, arg in options:
-        ##Read file for inputs
+    for opt, arg in optns:
         if opt in ("-f", "/f", "--file"):
-            fileLocation = arg
-
-        ##User: name provided
+            ##Read file for inputs
+            #fileLocation = arg
+            print("Yet to implement file-read feature")
         elif opt in ("-u", "/u", "--user"):
+            ##User: name provided
             code_user(arg) #Call code_user() and pass Redditor name
-
-        ##Subreddit: name provided
         elif opt in ("-s", "/s", "--subreddit"):
+            ##Subreddit: name provided
             code_subreddit(arg)
+    return
 
 def alternate():
     ##Menu if arguments are not given
@@ -46,7 +46,7 @@ def alternate():
             #Ask user for input of target name
             subredditName = input("Reddit thread: r/")
             code_subreddit(subredditName)
-        elif choice in ("e", "c", "s", "exit", "cancel", "stop"):
+        elif choice in ("e", "c", "exit", "cancel", "stop"):
             print("Exiting on user request...")
             sys.exit(0)
         elif choice in ("h", "help"):
@@ -71,6 +71,7 @@ def alternate():
             print("To select a subreddit, try: r, r/, or subreddit.")
             print("To exit, try: e, c, exit, cancel.")
             print()
+    return
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
